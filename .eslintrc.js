4a3a77a552ba6c0d5f8ee8ef63602944061df4ab
Kenneth Aladi <kennethmichael11@gmail.com>
2020-05-09 22:28:39 +0100

    fixed the lint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     node: true,
     jest: true
   },
-  extends: ['airbnb-base'],
+  extends: ['airbnb-base', 'eslint:recommended', 'plugin:react/recommended'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly'
