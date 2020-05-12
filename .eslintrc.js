4cfd9a69542a4653863b6b4ffd1182f1f748de6a
tonymj76 <demeyortj@gmail.com>
2020-05-12 13:56:05 +0100

    eslintrc config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,20 @@
 module.exports = {
+  parser: 'babel-eslint',
   env: {
     browser: true,
     es6: true,
     node: true,
     jest: true
   },
-  extends: ['airbnb-base'],
+  extends: ['airbnb-base:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: true,
+    },
+    sourceType: 'module',
+  },
+  plugins: ['react'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly'
@@ -16,6 +25,10 @@ module.exports = {
   },
   rules: {
     'comma-dangle': ['error', 'never'],
-    'no-param-reassign': ['error', { props: false }]
+    'no-param-reassign': ['error', { props: false }],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'no-console': ['warn', {allow: ['clear', 'info', 'error', 'dir', 'trace']}]
   }
 };
